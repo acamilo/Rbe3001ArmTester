6e1ba11670819786476531e668f5a523d4105ade
acamilo <alex.camilo@gmail.com>
2018-08-10 15:04:11 -0400

    Function to parse and unpac debug strings from device

---
+++ helper.py
@@ -0,0 +1,16 @@
+class CalcAvg:
+    avg = 0
+    avg_array = []
+    records = 20
+    def __init__(self,records=20):
+        self.avg=0
+        self.records = records
+        self.avg_array = [0.0] * self.records
+        
+    def add(self,value):
+        self.avg = 0
+        self.avg_array = [value] + self.avg_array[1:self.records-1]
+        for v in self.avg_array:
+            self.avg += v
+        self.avg = self.avg/self.records
+
